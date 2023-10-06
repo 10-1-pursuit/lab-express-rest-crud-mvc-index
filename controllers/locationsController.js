7673b6752cd58f2efaa01d8e7fd88655fede5c0a
xavierrice <xavier.rice@gmail.com>
2023-10-06 17:45:26 -0400

    attempting bonus-for some reason im people array is empty.

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -1,14 +1,28 @@
-const express = require('express');
+const express = require("express");
 const locations = express.Router();
-const locationArr = require('../models/locationModel')
+const locationArr = require("../models/locationModel");
+const personsArr = require("../models/personsModel");
 
+locations.get("/", (req, res) => {
+  res.json(locationArr);
+});
 
+locations.get("/people", (req, res) => {
+  const peopleLocations = locationArr.map((location) => {
+   
+    const filteredPeople = personsArr.filter((person) => {
+     person.mainLocation === location.zip;
+    });
+    console.log(filteredPeople)
 
-locations.get('/', (req, res) =>{
-    res.json(locationArr)
-})
-
-
-
+    return  {
+        ...location,
+        people: filteredPeople
+    };
+   
+  });
+ 
+  res.json(peopleLocations)
+});
 
 module.exports = locations;
