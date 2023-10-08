d60666f9eee05b5144e4550d32395c653bb9fa2f
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-10-08 17:21:38 -0400

    bonus

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const locations = express.Router();
 const locationsArr = require("../models/location.model");
+const personsArr = require("../models/person.model");
 
 locations.get("/", (req,res) => {
     res.json(locationsArr);
+})
+
+locations.get("/people", (req,res) => {
+    const organizedData = [];
+    for (const location of locationsArr) {
+        const locationWithPeople = {...location, people: []};
+        const peopleAtLocation = personsArr.filter((p) => p.mainLocation === location.zip);
+        locationWithPeople.people = peopleAtLocation;
+        organizedData.push(locationWithPeople);
+    }
+    res.json(organizedData)
 })
 
 module.exports = locations;
