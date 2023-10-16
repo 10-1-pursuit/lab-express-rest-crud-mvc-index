b44c82384a9a01dd7ad97cae0d46ae59c6f79792
Salomon Balthazar <131703377+tru-salomon@users.noreply.github.com>
2023-10-16 04:39:56 -0400

    routes

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const locationsController = require("./controllers/locations.controller.js")
+const machinesController = require("./controllers/machines.controller.js")
+const personsController = require("./controllers/persons.controller.js")
+const plansController = require("./controllers/plans.controller.js")
+const special_eventsController = require("./controllers/special-events.controller.js")
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use("/locations", locationsController)
+
+app.use("/people", personsController)
+
+app.use("/equipment", machinesController)
+
+app.use("/plans", plansController)
+
+
+app.use("/special-events", special_eventsController)
+
 
 module.exports = app;
