ce95259c4732553f212a5a330f301efdd0ae923e
Kenneth-Parker <kennethkavon@gmail.com>
2023-10-21 03:57:05 -0400

    bonus, zip correction

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model.js");
+const people = require("../models/person.model.js");
 
 locations.get("/locations", (req, res) => {
-  res.send(locationsArray);
+    res.send(locationsArray);
 });
+
+locations.get("/locations/people", (req, res) => {
+    const allData = locationsArray.map((location) => {
+      const peopleAtLocation = people.filter((person) => person.mainLocation === location.zip);
+      return {
+        street: location.street,
+        city: location.city,
+        state: location.state,
+        zip: location.zip,
+        people: peopleAtLocation,
+      };
+    });
+    
+    res.json(allData);
+  });
+  
 
 module.exports = locations;
