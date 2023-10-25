e2515353547e4d8ab4e17e4ce2c9dfd274a8d382
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-10-25 01:07:32 -0400

    modified app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+const app= express();
 
 const locationsControllers = require("./controllers/locations.controllers");
 const machinesControllers = require("./controllers/machines.controllers");
