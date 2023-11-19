f34fb99556c636baf19e7b3843f5ee852bc26331
keniaDE <decoteaukenia@gmail.com>
2023-11-19 06:49:46 -0500

    error message complete

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const personController = require('./controllers/persons.controller');
 app.use('/persons', personController)
 
 const plansController = require('./controllers/plans.controller');
-app.use('/plans', plansConrtroller) 
+app.use('/plans', plansController) 
 
 const specialEventsController = require('./controllers/specialEvent.controller');
 app.use('/specialEvents', specialEventsController) 
@@ -25,6 +25,12 @@ app.get('/', (req,res) =>{
     res.send('Hello, world!')
 })
 
+
+ // 404 PAGE
+ app.get("*", (req, res) => {
+    res.status(404).json({ error: " Sorry, no page found" });
+  });
+  
 
 // EXPORT
 module.exports = app;
